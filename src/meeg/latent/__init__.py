0ef04a12255514200022e2e407db048be0b9809e
Ladislas Nalborczyk <lnalborczyk@users.noreply.github.com>
2024-02-06 13:21:00 +0100

    uploading a function to compute speed and curvature of trajectories

--- src/meeg/latent/__init__.py
+++ src/meeg/latent/__init__.py
@@ -3,3 +3,4 @@ from .embeddings import perform_mds
 from .embeddings import compute_similarity
 from .trajectories import pca_through_time
 from .trajectories import compare_pca_through_time
+from .trajectories import stats_trajectories
--- src/meeg/latent/trajectories.py
+++ src/meeg/latent/trajectories.py
@@ -6,8 +6,20 @@ from meeg.decoding import prep_data_for_decoding
 
 
 def pca_through_time(epochs, n_components=10):
-    
-    # reshaping data
+    """
+    Computing PCA through time (trajectories).
+
+    Parameters
+    ----------
+    epochs: MNE epochs
+        The M/EEG data from which to compute the trajectory.
+        Should be a 3-dimensional array/tensor of shape trials x channels x time_steps.
+
+    n_components: int
+        Number of PCA components to include.
+    """
+        
+    # computing the PCA through time
     X, y = prep_data_for_decoding(
         epochs=epochs,
         pca=True, n_components=n_components,
@@ -28,6 +40,22 @@ def pca_through_time(epochs, n_components=10):
 
 
 def compare_pca_through_time(epochs1, epochs2, n_components=10):
+    """
+    Computing PCA through time (trajectories) for two epochs in a common space.
+
+    Parameters
+    ----------
+    epochs1: MNE epochs
+        The M/EEG data from which to compute the trajectory.
+        Should be a 3-dimensional array/tensor of shape trials x channels x time_steps.
+
+    epochs2: MNE epochs
+        The M/EEG data from which to compute the trajectory.
+        Should be a 3-dimensional array/tensor of shape trials x channels x time_steps.
+
+    n_components: int
+        Number of PCA components to include.
+    """
     
     # computing the global pca
     concatenated_epochs=mne.concatenate_epochs(
@@ -52,4 +80,63 @@ def compare_pca_through_time(epochs1, epochs2, n_components=10):
     
     # returning it
     return x_pca1, x_pca_std1, x_pca2, x_pca_std2
+
+def stats_trajectories(epochs, n_components=10):
+    """
+    Computing mean, std, speed, and curvature of latent trajectory.
+
+    Parameters
+    ----------
+    epochs: MNE epochs
+        The M/EEG data from which to compute the trajectory.
+        Should be a 3-dimensional array/tensor of shape trials x channels x time_steps.
+
+    n_components: int
+        Number of PCA components to include.
+    """
+    
+    # reshaping data
+    X, y = prep_data_for_decoding(
+        epochs=epochs,
+        pca=True, n_components=n_components,
+        moving_average=False, kernel_size=5,
+        moving_average_with_decim=False, decim=4,
+        trials_averaging=False, ntrials=4, shuffling_or_not=True
+    )
+    
+    # averaging these data across trials
+    pca_mean = np.mean(X, axis=0).transpose()
+    pca_std = np.std(X, axis=0).transpose()
+
+    # computing the derivatives of each dimension (i.e., velocity)
+    x_t = np.gradient(pca_mean[:, 0])
+    y_t = np.gradient(pca_mean[:, 1])
+    z_t = np.gradient(pca_mean[:, 2])
+
+    # retrieving velocity for each dimension
+    velocity = np.array([[x_t[i], y_t[i], z_t[i]] for i in range(x_t.size)])
+
+    # computing speed (modulus of the velocity)
+    speed = np.sqrt(x_t * x_t + y_t * y_t + z_t * z_t)
+
+    # computing the tangent, we will perform some transformation which will ensure that the size of the speed and velocity is the same.
+    # also, we need to be able to divide the vector-valued velocity function to the scalar speed array.
+    # https://www.delftstack.com/howto/numpy/curvature-formula-numpy/
+    # tangent = np.array([1/speed] * 3).transpose() * velocity
+
+    # computing the curvature
+    # ss_t = np.gradient(speed)
+    xx_t = np.gradient(x_t)
+    yy_t = np.gradient(y_t)
+    zz_t = np.gradient(z_t)
+    curvature= np.abs(xx_t * y_t - x_t * yy_t) / (x_t * x_t + y_t * y_t) ** 1.5
+
+    # computing the acceleration
+    # https://stackoverflow.com/questions/28269379/curve-curvature-in-numpy
+    # t_component = np.array([ss_t] * 3).transpose()
+    # n_component = np.array([curvature * speed * speed] * 3).transpose()
+    # acceleration = t_component * tangent + n_component * normal
+
+    # returning it
+    return pca_mean, pca_std, speed, curvature
 
